d2e5eca997cbb64f6619aba4acacdfac8f33eb6f
homonnom <josojoso0702@gmail.com>
2020-01-18 03:04:39 +0900

    final

--- middlewares.js
+++ middlewares.js
@@ -1,7 +1,22 @@
 import routes from "./routes";
 
 export const localMiddleware = (req, res, next) => {
-  res.locals.siteName = "Hyeonjin Kim";
   res.locals.routes = routes;
+  res.locals.address = "http://localhost:4000";
+  res.locals.siteName = "Hyeonjin Kim";
+  res.locals.area = "Seoul";
+  res.locals.email = "wlskhj@icloud.com";
+  res.locals.genre = "Fabric art";
+  res.locals.first = "familiar, unfamiliar";
+  res.locals.second = "I.L.T";
+  res.locals.third = "in oneself";
+  res.locals.fourth = "a questioning mind";
+  res.locals.fifth = "a swell ";
+  res.locals.sixth = "floating village";
+  res.locals.seventh = "about incompleteness";
+  res.locals.eighth = "tool kits for me";
+  res.locals.ninth = "personal space";
+  res.locals.tenth = "leave prints";
+  res.locals.eleventh = "full bloom";
   next();
 };
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+
 const autoprefixer = require("autoprefixer");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
